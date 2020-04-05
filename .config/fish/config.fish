0f7a71a7706e7de44776e84435a45d2b5acc605b
Marco Andreolli <drazde@gmail.com>
2020-04-05 15:37:54 +0200

    Add fish config file

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+#if exa --version >/dev/null
+#	set -U fish_user_abbreviations $fish_user_abbreviations 'l=exa'
+#	set -U fish_user_abbreviations $fish_user_abbreviations 'ls=exa'
+#	set -U fish_user_abbreviations $fish_user_abbreviations 'll=exa -l'
+#	set -U fish_user_abbreviations $fish_user_abbreviations 'lll=exa -la'
+#else
+#	set -U fish_user_abbreviations $fish_user_abbreviations 'l=ls'
+#	set -U fish_user_abbreviations $fish_user_abbreviations 'll=ls -l'
+#	set -U fish_user_abbreviations $fish_user_abbreviations 'lll=ls -la'
+#end
+
+# show inf on my so
+#neofetch
+
+function mp3
+    youtube-dl --continue --ignore-errors --add-metadata --embed-thumbnail --extract-audio --audio-format mp3 $argv;
+end;
+
+function mount-android
+    go-mtpfs /mnt/key
+end
+function umount-android
+    fusermount /mnt/key
+end;
+
+function config 
+    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
+end;

74cfd1f0a8dfbf3bebb475b0166cecd27d9d630c
Marco Andreolli <drazde@gmail.com>
2021-04-11 22:05:27 +0200

    nvim config

--- .config/nvim/lua/nv-nvimtree/init.lua
+++ .config/nvim/lua/nv-nvimtree/init.lua
@@ -22,6 +22,10 @@ vim.g.nvim_tree_indent_markers = 1 --"0 by default, this option shows indent mar
 vim.g.nvim_tree_follow = 1 --"0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_auto_close = O.auto_close_tree --0 by default, closes the tree when it's the last window
 vim.g.nvim_tree_auto_ignore_ft = 'startify' --"empty by default, don't auto open tree on specific filetypes.
+
+--TODO
+--vim.g:nvim_tree_root_folder_modifier = '%:p:h' --This is the default. See :help filename-modifiers for more options
+
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 vim.g.nvim_tree_bindings = {
     -- mappings

925cbae24398cb9fa3088bb54c1f4bb255047c63
Marco Andreolli <drazde@gmail.com>
2021-04-27 10:44:35 +0200

    neovim config (converting)

---
+++ .config/nvim/lua/folding.lua
@@ -0,0 +1,9 @@
+-- Folding
+vim.cmd('set foldlevelstart=20')
+vim.cmd('let g:xml_syntax_folding=1')
+ 
+vim.api.cmd([[
+au FileType xml setlocal foldmethod=syntax
+au FileType javascript setlocal foldmethod=syntax
+au FileType vim set foldmethod=marker
+]])

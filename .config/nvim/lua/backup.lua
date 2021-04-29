e81525971860ed26b6b18cb8920510e832507b30
Marco Andreolli <drazde@gmail.com>
2021-04-29 16:38:09 +0200

    neovim config

--- .config/nvim/lua/backup.lua
+++ .config/nvim/lua/backup.lua
@@ -7,6 +7,13 @@
 -- "    autocmd VIMEnter * :source ~/.vim/tmp/session.vim
 -- "endif
 -- "autocmd VIMLeave * :mksession! ~/.vim/tmp/session.vim
+local function convert_path(s)
+    if vim.fn.has('win32') == 1 then
+        s = s:gsub("/", "\\");
+        s = s:gsub(" ", "\\ ");
+    end
+    return s
+end
 
 vim.cmd('set backup')             -- Enable backup
 vim.cmd('set undofile')           -- it's 2013, Vim.
@@ -15,11 +22,15 @@ vim.cmd('set noswapfile')
 vim.cmd('set undolevels=1000')
 vim.cmd('set undoreload=10000')
 
--- Only on linux I change "temps" dir.. 
+local undodir = vim.env.HOME .. '/.cache/nvim/tmp/undo/';    -- Undo files
+local backupdir=vim.env.HOME ..'/.cache/nvim/tmp/backup/'; -- Backups
+local directory=vim.env.HOME .. '/.cache/nvim/tmp/swap/';   -- Swap file
+
+--undodir = convert_path(undodir);
+--backupdir = convert_path(backup);
+--directory = convert_path(directory);
+
 if vim.fn.has('unix') == 1 then
-    local undodir=vim.env.HOME .. '/.cache/nvim/tmp/undo/';    -- Undo files
-    local backupdir=vim.env.HOME ..'/.cache/nvim/tmp/backup/'; -- Backups
-    local directory=vim.env.HOME .. '/.cache/nvim/tmp/swap/';   -- Swap file
     
     vim.cmd('set undodir=' .. undodir);     -- undo files
     vim.cmd('set backupdir=' .. backupdir); -- backups
@@ -36,4 +47,13 @@ if vim.fn.has('unix') == 1 then
     if vim.fn.isdirectory(directory) ~= 1 then
         vim.fn.mkdir(directory,"p")
     end
-end
+else 
+    undodir = 'set undodir=' .. convert_path(undodir);
+    vim.cmd(undodir);     -- undo files
+
+    backupdir = 'set backupdir=' .. convert_path(backupdir); 
+    vim.cmd(backupdir);     -- backup files
+
+    directory = 'set directory=' .. convert_path(directory); 
+    vim.cmd(directory);     -- swap files
+end;

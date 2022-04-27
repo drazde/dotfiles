670aca9784f47e9f1698cf8214c467c7e6f5b728
Marco Andreolli <drazde@gmail.com>
2022-04-27 09:52:12 +0200

    update nvimtree config

--- .config/nvim/lua/nv-nvimtree/init.lua
+++ .config/nvim/lua/nv-nvimtree/init.lua
@@ -18,7 +18,7 @@ let g:nvim_tree_show_icons = {
 -- vim.g.nvim_tree_disable_netrw = 0 --"1 by default, disables netrw
 -- vim.g.nvim_tree_hijack_netrw = 0 --"1 by default, prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
 -- vim.g.nvim_tree_hide_dotfiles = 1 --0 by default, this option hides files and folders starting with a dot `.`
-vim.g.nvim_tree_indent_markers = 1 --"0 by default, this option shows indent markers when folders are open
+-- vim.g.nvim_tree_indent_markers = 1 --"0 by default, this option shows indent markers when folders are open
 -- vim.g.nvim_tree_follow = 1 --"0 by default, this option allows the cursor to be updated when entering a buffer
 -- vim.g.nvim_tree_auto_close = O.auto_close_tree --0 by default, closes the tree when it's the last window
 vim.g.nvim_tree_auto_ignore_ft = 'startify' --"empty by default, don't auto open tree on specific filetypes.
@@ -29,10 +29,8 @@ vim.g.nvim_tree_auto_ignore_ft = 'startify' --"empty by default, don't auto open
 
 -- each of these are documented in `:help nvim-tree`
 require'nvim-tree'.setup { 
-  auto_close = false,
   auto_reload_on_write = true,
   disable_netrw = false,
-  hide_root_folder = false,
   hijack_cursor = false,
   hijack_netrw = true,
   hijack_unnamed_buffer_when_opening = false,
